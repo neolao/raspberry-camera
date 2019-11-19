a1d0ea43271430ee5fd2c8e86eb94b23b4c10a20
neolao <contact@neolao.com>
2019-11-19 21:28:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# See https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-convenience-script
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker pi
