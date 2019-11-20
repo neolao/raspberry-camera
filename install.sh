e73b86e7d91a1776401162a80e39391f42359a3f
neolao <contact@neolao.com>
2019-11-20 12:53:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # See https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-convenience-script
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-usermod -aG docker pi
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+#usermod -aG docker pi
 
 ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+
+apt-get install -y git libv4l-dev libjpeg8-dev imagemagick cmake
+git clone https://github.com/jacksonliam/mjpg-streamer.git ~/mjpg-streamer
+cd ~/mjpg-streamer/mjpg-streamer-experimental
+make USE_LIBV4L2=true clean all
+make DESTDIR=/usr install
