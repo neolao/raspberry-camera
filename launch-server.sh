4e383caa1c491ea63de9cab11277c640dbdbee43
neolao <contact@neolao.com>
2019-11-20 12:35:11 +0000

    p

--- launch-server.sh
+++ launch-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MJPG_STREAM_WWW=~/mjpg-streamer/mjpg-streamer-experimental/www/
+MJPG_STREAM_WWW=/home/pi/mjpg-streamer/mjpg-streamer-experimental/www/
 export LD_LIBRARY_PATH=/usr/local/lib
 
 mjpg_streamer -i "input_file.so -f /run/stream -n camera.jpg" -o "output_http.so -w $MJPG_STREAM_WWW"
