8247782a336a780e5ea637c5b550ca1759dc99e7
neolao <contact@neolao.com>
2019-11-20 12:54:21 +0000

    increase quality

--- capture-camera.sh
+++ capture-camera.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-raspistill --nopreview -w 640 -h 480 -q 5 -o /run/stream/camera.jpg -tl 500 -t 9999999
+raspistill --nopreview -w 800 -h 600 -q 90 -o /run/stream/camera.jpg -tl 100 -t 9999999
